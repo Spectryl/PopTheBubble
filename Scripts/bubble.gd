aa54dd768958d0cce9594fe56e5d2f953537ed45
Sonu <156054782+Spectryl@users.noreply.github.com>
2025-09-26 12:49:23 -0500

    add: Angular Rotation to bubbles

--- Scripts/bubble.gd
+++ Scripts/bubble.gd
@@ -2,3 +2,11 @@ class_name Bubble
 extends RigidBody2D
 
 @export var BUBBLE_SPEED : int
+
+@onready var sprite = get_node("Sprite2D")
+
+
+
+func _on_body_entered(body: Node) -> void:
+	if body.is_in_group("Poisonable"):
+		pass
